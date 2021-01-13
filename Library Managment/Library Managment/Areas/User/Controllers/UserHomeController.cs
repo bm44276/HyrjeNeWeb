f1b0f1242749ff2e38f51c71e0ed26e1c094928d
mk45852 <mk45852@ubt-uni.net>
2021-01-13 21:48:13 +0100

    Index page, header fixed, footer fixed, basically wip

--- Library Managment/Library Managment/Areas/User/Controllers/UserHomeController.cs
+++ Library Managment/Library Managment/Areas/User/Controllers/UserHomeController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Library_Managment.Data;
+using Library_Managment.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +13,24 @@ namespace Library_Managment.Areas.User.Controllers {
     [Area("User")]
     [Authorize]
     public class UserHomeController : Controller {
+        private readonly ApplicationDbContext _context;
+        
+        public UserHomeController(ApplicationDbContext context) {
+            _context = context;
+        }
+
         public IActionResult Index() {
+
+     
+            List<Book> NewestBooks = (List<Book>)_context.Books.OrderByDescending(x => x.Id).Take(8).ToList<Book>();
+            ViewBag.NewestBooks = NewestBooks;
+
+            return View();
+        }
+
+        public IActionResult BookHistory() {
+
+
             return View();
         }
     }
