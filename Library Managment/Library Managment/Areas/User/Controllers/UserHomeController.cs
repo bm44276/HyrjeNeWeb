ee51ca909b0096e223109c3322f8766c1bf2f63d
mk45852 <mk45852@ubt-uni.net>
2021-01-17 17:51:46 +0100

    book history new design, index pagination, filtering in progress

--- Library Managment/Library Managment/Areas/User/Controllers/UserHomeController.cs
+++ Library Managment/Library Managment/Areas/User/Controllers/UserHomeController.cs
@@ -10,6 +10,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TakenBooks = Library_Managment.Areas.User.Models.TakenBooks;
+using Library_Managment.Data.Migrations;
+using Library_Managment.Models;
+using Library_Managment.Areas.Admin.Models;
+
 
 namespace Library_Managment.Areas.User.Controllers {
 
@@ -23,13 +27,40 @@ namespace Library_Managment.Areas.User.Controllers {
             _userManager = userManager;
         }
 
-        public IActionResult Index() {
+        public async Task<IActionResult> IndexAsync(string sortOrder, string currentFilter,string searchString, int? pageNumber) {
 
      
             List<Book> NewestBooks = (List<Book>)_context.Books.OrderByDescending(x => x.Id).Take(8).ToList<Book>();
             ViewBag.NewestBooks = NewestBooks;
 
-            return View();
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["Author"] = sortOrder == "author_desc" ? "author_asc" : "author_desc";
+            ViewData["Date"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+            var books = from s in _context.Books
+                        select s;
+            switch (sortOrder) {
+                case "name_desc":
+                    books = books.OrderBy(s => s.Name);
+                    break;
+                case "author_desc":
+                    books = books.OrderByDescending(s => s.Author);
+                    break;
+                case "author_asc":
+                    books = books.OrderBy(s => s.Author);
+                    break;
+                case "date_desc":
+                    books = books.OrderByDescending(s => s.PublishDate);
+                    break;
+                case "date_asc":
+                    books = books.OrderBy(s => s.PublishDate);
+                    break;
+                default:
+                    books = books.OrderBy(s => s.ISBN);
+                    break;
+            }
+
+            int pageSize = 8;
+            return View(await PaginatedList<Book>.CreateAsync(books.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         public async Task<IActionResult> BookHistoryAsync() {
